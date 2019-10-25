a42e80b83e3f4a50d00b46ba21e8643785a8f418
davejmacm <davejmacm@gmail.com>
2019-10-26 00:24:12 +0100

    feature/added api call in meme generator component

--- src/Components/memeGenerator.js
+++ src/Components/memeGenerator.js
@@ -1,16 +1,24 @@
 import React, {Component} from 'react';
 
-
-
 class MemeGenerator extends Component {
     constructor() {
         super()
         this.state = {
             topText: "",
             bottomText: "",
-            randomImg: "http://i.imgflip.com/1bij.jpg"
+            randomImg: "http://i.imgflip.com/1bij.jpg",
+            allMemeImgs: []
         }
     };
+
+    componentDidMount(){
+        fetch("https://api.imgflip.com/get_memes")
+            .then(response => response.json())
+            .then(response => {
+                const {memes} = response.data
+                this.setState({ allMemeImgs: memes })
+        })
+    }
 
     render() {
         return(
