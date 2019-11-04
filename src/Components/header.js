f69f4eb65b46b1e99bae05713cfbebba875a6373
davejmacm <davejmacm@gmail.com>
2019-11-04 22:29:44 +0000

    feature/ added animation for header; chili image won't change

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const Header = () =>{
-    return(
-        <header className="App-header">
-            <img src="https://i.redd.it/vx4qd4r5wda11.png" alt="hot chilli"/>
-       Who ordered some SPICY MEMES!?
-      </header>
-    );
-}
+const Header = () => {
+  return (
+    <header className="App-header">
+      <img
+        className="chili-icon"
+        src="https://i.redd.it/vx4qd4r5wda11.png"
+        alt="hot chilli"
+      />
+      Who ordered some SPICY MEMES!?
+    </header>
+  );
+};
 
 export default Header;
